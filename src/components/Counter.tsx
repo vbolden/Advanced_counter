54ab046941d54182357247423b08f4b188a5aeef
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-28 18:41:18 -0400

    added useStates and UI render

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
 
 function Counter () {
+    // USE STATES FOR COUNT AND STEP VALUE
+    const [count, setCount] = useState(0)
+    const [stepValue, setStepValue] = useState<number>(1)
 
+    return (
+        <div className="counter">
+            <p>counter</p>
+            <h1>Current Count: {count}</h1>
 
-    return
+            <div className="button-container">
+                <button>Increment</button>
+                <button>Decrement</button>
+                <button>Reset</button>
+            </div>
+
+            <label> Step Value: 
+                <input type="number" value={stepValue} />
+            </label>
+
+            <div className="count-history">
+                <p>Count History: </p>
+            </div>
+        </div>
+    )
 }
 
 export default Counter;
