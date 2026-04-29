cfff26199a76e2070cc2557f2a7d317438231d7a
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-29 18:09:20 -0400

    updated count useState to return 0 is no history is found, fixed useEffect to add count to history and save in local storage

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,20 +1,36 @@
 import { useState, useEffect } from "react";
 
-function Counter () {
+function Counter() {
     // USE STATES FOR COUNT AND STEP VALUE
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(() => {
+        const saved = localStorage.getItem("countHistory");
+        if(saved) {
+            const parsed = JSON.parse(saved);
+            return parsed[parsed.length - 1]; 
+        } return 0
+    });
     const [stepValue, setStepValue] = useState<number>(1);
     // USE STATE FOR COUNT HISTORY
-    const [history, setHistory] = useState<string[]>([]);
+    const [history, setHistory] = useState<number[]>(() => {
+        const saved = localStorage.getItem('countHistory');
+        return saved ? JSON.parse(saved) : [];
+    });
 
     useEffect(() => {
-        
-    })
+        setHistory((prevHistory) => {
+            if(prevHistory[prevHistory.length - 1] === count) {
+                return prevHistory // PREVENTS DUPLICATES
+            }
+
+            const updated = [...prevHistory, count];
+            localStorage.setItem("countHistory", JSON.stringify(updated));
+            return updated;
+        })
+    }, [count]);
 
     // FUNCTIONS FOR INCREMENT, DECREMENT, AND RESET COUNT
     const add = () => {
         setCount(count + stepValue)
-        
     }
 
     const sub = () => {
@@ -25,6 +41,10 @@ function Counter () {
     const reset = () => {
         setCount(0)
         setStepValue(1)
+
+        const resetHistory = [0]
+        setHistory(resetHistory)
+        localStorage.setItem("countHistory", JSON.stringify(resetHistory))
     }
 
     // HANDLE STEP VALUE CHANGE
@@ -45,12 +65,19 @@ function Counter () {
                 <button onClick={reset} className="reset">Reset</button>
             </div>
 
-            <label> Step Value: 
+            <label> Step Value:
                 <input type="number" value={stepValue} onChange={handleStepValue} />
             </label>
 
             <div className="count-history">
                 <p>Count History: </p>
+                <hr />
+                <div className="history">
+                    <ul>
+                        {history.map((value, index) =>
+                            <li key={index}>{value}</li>)}
+                    </ul>
+                </div>
             </div>
         </div>
     )
